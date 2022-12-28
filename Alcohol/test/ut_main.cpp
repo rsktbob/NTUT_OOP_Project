59eb718edda2c849f7291fb8ad59ed1338802129
“呂凱達” <t110590038@ntut.edu.tw>
2022-12-28 16:14:59 +0800

    delete alcohol

--- Alcohol/test/ut_main.cpp
+++
@@ -1,9 +0,0 @@
-#include <gtest/gtest.h>
-#include "ut_alcohol.h"
-#include "ut_sour.h"
-#include "ut_tequila.h"
-
-int main(int argc, char *argv[]) {
-	testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
-}
